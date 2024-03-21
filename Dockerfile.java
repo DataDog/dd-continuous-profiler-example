4903ad451ee43d53ea8f0356cb6bd773d1807e52
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-03-21 15:50:49 +0000

    Fix/modernize Java docker file

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,7 +1,7 @@
-FROM openjdk:14
+FROM sapmachine:21-jdk-ubuntu-22.04
 
 COPY . /home
 WORKDIR /home/java
-RUN curl -L -o dd-java-agent.jar 'https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.datadoghq&a=dd-java-agent&v=0.69.0'
+RUN curl -L -o dd-java-agent.jar 'https://dtdg.co/latest-java-tracer'
 RUN ./gradlew --no-daemon installDist
 CMD JAVA_OPTS="-Ddd.agent.host=$DD_AGENT_HOST -Ddd.profiling.enabled=true -javaagent:dd-java-agent.jar" ./build/install/movies-api-java/bin/movies-api-java
