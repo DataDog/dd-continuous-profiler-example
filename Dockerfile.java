43f84c38df20d4e19ee1909b2dae60e2d06964e0
Ivo Anjo <ivo.anjo@datadoghq.com>
2024-03-21 17:00:41 +0000

    Add curl to `Dockerfile.java`
    This is needed to install the agent!

--- Dockerfile.java
+++ Dockerfile.java
@@ -2,6 +2,7 @@ FROM sapmachine:21-jdk-ubuntu-22.04
 
 COPY . /home
 WORKDIR /home/java
+RUN apt update && apt install -y curl
 RUN curl -L -o dd-java-agent.jar 'https://dtdg.co/latest-java-tracer'
 RUN ./gradlew --no-daemon installDist
 CMD JAVA_OPTS="-Ddd.agent.host=$DD_AGENT_HOST -Ddd.profiling.enabled=true -javaagent:dd-java-agent.jar" ./build/install/movies-api-java/bin/movies-api-java
